34d0a22765c2104f7a019026a7882f8184657c69
exyq <ericxingy@163.com>
2022-12-23 00:38:16 +0800

    upload

--- co/price.c
+++ co/price.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main()
 {
-	int price = 0;
+	int price;
+	int bill; 
+	printf("please your money(enn): ");
+	scanf("%d", &bill);
 	printf("Please enter the price(enn): ");
 	scanf("%d", &price);
 
-	int change = 100 - price;
+	if (bill >= price) {
+		int change = bill - price;
 
-	printf("return your money%d(enn)\n", change);
+		printf("return your money%d(enn)\n", change);
+	}
+	else {
+		printf("your money not enough!\n");
+	}
 }
